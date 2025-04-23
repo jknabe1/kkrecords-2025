dc816befca04cfe66f3f02585b20b4f20882edd8
Jens Knabe <jens.knabe@kkmedia.se>
2025-04-23 19:15:57 +0200

    Remove CookieNotice component from RootLayout for cleanup

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,7 +50,6 @@ export default function RootLayout({ children,}: Readonly<{children: React.React
         <Header/>  
           <LenisScrollProvider>
             {children}
-            <CookieNotice />
             <SpeedInsights/>
             <Analytics />
           </LenisScrollProvider>
