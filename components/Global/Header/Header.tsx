62ed77ad00931c3ede1f47afc7ba86be2d96f433
Jens Knabe <jens.knabe@kkmedia.se>
2026-04-05 22:41:16 +0200

    refactor: remove dark mode classes from header component for improved styling consistency

--- components/Global/Header/Header.tsx
+++ components/Global/Header/Header.tsx
@@ -73,7 +73,7 @@ const Header = () => {
     <>
       <header
         aria-label="Main Navigation"
-        className="sticky top-0 left-0 right-0 h-[60px] lg:h-[70px] bg-white dark:bg-neutral-900 dark:border-neutral-700 border-b-black border-b border-solid flex flex-col justify-center z-50 overflow-hidden"
+        className="sticky top-0 left-0 right-0 h-[60px] lg:h-[70px] bg-white border-b-black border-b border-solid flex flex-col justify-center z-50 overflow-hidden"
         role="navigation"
       >
         <div className="px-2 py-8 lg:px-5">
@@ -125,6 +125,101 @@ const Header = () => {
                 </picture>
               </Link>
             </div>
+            <div className="col-span-8 lg:col-span-4">
+              <ul className="flex justify-end items-center lg:items-end">
+                <li>
+                  <button className="block px-[7px]" aria-label="Open Search" onClick={toggleSearch}>
+                    <div className="h-[70px] flex flex-col justify-center hidden lg:flex">
+                      <div className="relative">
+                        <svg width="36" height="37" viewBox="0 0 36 37" className="icon-fill">
+                          <path d="M22.1659 23.1292L34.3457 35.9806" stroke="currentColor" strokeWidth="2"></path>
+                          <path
+                            d="M26.1024 13.4297C26.1024 20.233 20.6487 25.7298 13.9436 25.7298C7.23852 25.7298 1.78479 20.233 1.78479 13.4297C1.78479 6.62649 7.23852 1.12964 13.9436 1.12964C20.6487 1.12964 26.1024 6.62649 26.1024 13.4297Z"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            className="icon-fill-fill"
+                          ></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <svg className="lg:hidden" width="27" height="29" viewBox="0 0 27 29">
+                      <path d="M16.8905 18.0781L26.3168 28.0241" stroke="currentColor"></path>
+                      <path
+                        d="M20.2111 10.5715C20.2111 15.9852 15.8705 20.3648 10.5272 20.3648C5.18393 20.3648 0.843262 15.9852 0.843262 10.5715C0.843262 5.15775 5.18393 0.778198 10.5272 0.778198C15.8705 0.778198 20.2111 5.15775 20.2111 10.5715Z"
+                        stroke="currentColor"
+                        fill="none"
+                      ></path>
+                    </svg>
+                  </button>
+                </li>
+                <li>
+                  <Link className="block px-[7px] lg:pr-0" title="Backstage" href="/backstage">
+                    <div className="h-[70px] flex flex-col justify-center hidden lg:flex">
+                      <div className="relative">
+                        <svg width="35" height="36" viewBox="0 0 35 36" className="icon-fill">
+                          <circle
+                            cx="17.6423"
+                            cy="10.1467"
+                            r="8.73657"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                            fill="none"
+                            className="icon-fill-fill"
+                          ></circle>
+                          <path
+                            fillRule="evenodd"
+                            clipRule="evenodd"
+                            d="M32.6461 35.7288C32.6659 35.414 32.676 35.0965 32.676 34.7767C32.676 26.5491 26.0062 19.8794 17.7787 19.8794C9.55111 19.8794 2.88135 26.5491 2.88135 34.7767C2.88135 35.0965 2.89143 35.414 2.91128 35.7288H0.907722C0.890217 35.4137 0.881348 35.0962 0.881348 34.7767C0.881348 25.4446 8.44654 17.8794 17.7787 17.8794C27.1108 17.8794 34.676 25.4446 34.676 34.7767C34.676 35.0962 34.6671 35.4137 34.6496 35.7288H32.6461Z"
+                          ></path>
+                          <circle cx="17.5" cy="35" r="16" fill="none" className="icon-fill-fill"></circle>
+                          <rect x="2" y="34" height="2" width="32" fill="currentColor"></rect>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="text-center lg:hidden relative bottom-[2px]">
+                      <svg width="23" height="29" viewBox="0 0 35 36" className="icon-fill">
+                        <circle
+                          cx="17.6423"
+                          cy="10.1467"
+                          r="8.73657"
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          fill="none"
+                          className="icon-fill-fill"
+                        ></circle>
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M32.6461 35.7288C32.6659 35.414 32.676 35.0965 32.676 34.7767C32.676 26.5491 26.0062 19.8794 17.7787 19.8794C9.55111 19.8794 2.88135 26.5491 2.88135 34.7767C2.88135 35.0965 2.89143 35.414 2.91128 35.7288H0.907722C0.890217 35.4137 0.881348 35.0962 0.881348 34.7767C0.881348 25.4446 8.44654 17.8794 17.7787 17.8794C27.1108 17.8794 34.676 25.4446 34.676 34.7767C34.676 35.0962 34.6671 35.4137 34.6496 35.7288H32.6461Z"
+                        ></path>
+                        <circle cx="17.5" cy="35" r="16" fill="none" className="icon-fill-fill"></circle>
+                        <rect x="2" y="34" height="2" width="32" fill="currentColor"></rect>
+                      </svg>
+                    </div>
+                  </Link>
+                </li>
+                <li className="lg:hidden">
+                  <button
+                    className="block pl-[7px]"
+                    aria-label={isNavVisible ? "Close Menu" : "Open Menu"}
+                    onClick={isNavVisible ? closeNav : openNav}
+                  >
+                    {isNavVisible ? (
+                      <svg width="23" height="23" viewBox="0 0 23 23">
+                        <path d="M1 1L22 22" stroke="currentColor" strokeWidth="1.5"></path>
+                        <path d="M22 1L1 22" stroke="currentColor" strokeWidth="1.5"></path>
+                      </svg>
+                    ) : (
+                      <svg width="23" height="23" viewBox="0 0 23 23">
+                        <rect width="23" height="1" fill="currentColor"></rect>
+                        <rect y="11" width="23" height="1" fill="currentColor"></rect>
+                        <rect y="22" width="23" height="1" fill="currentColor"></rect>
+                      </svg>
+                    )}
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </header>
@@ -136,14 +231,14 @@ const Header = () => {
         }`}
         ref={searchRef}
       >
-        <div className="relative w-full bg-white dark:bg-neutral-900 dark:border-neutral-700 border-b border-gray-200 transform transition-transform duration-300 ease-out">
-          <div className="flex items-center h-[160px] md:px-10 dark:border-neutral-700 border-b border-black border-solid px-2 py-3 lg:px-5">
+        <div className="relative w-full bg-white border-b border-gray-200 transform transition-transform duration-300 ease-out">
+          <div className="flex items-center h-[160px] md:px-10 border-b border-black border-solid px-2 py-3 lg:px-5">
             <button
-              className="mr-4 text-black dark:text-white hover:text-gray-900 dark:hover:text-gray-300 transition-colors"
+              className="mr-4 text-black hover:text-gray-900 transition-colors"
               aria-label="Close search"
               onClick={toggleSearch}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
                 <path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"></path>
               </svg>
             </button>
@@ -156,16 +251,16 @@ const Header = () => {
               onKeyDown={(e) => {
                 if (e.key === "Enter") handleSearchSubmit()
               }}
-              className="flex-1 text-2xl md:text-3xl lg:text-4xl font-light text-black dark:text-white placeholder-gray-300 dark:placeholder-gray-600 bg-transparent border-none outline-none uppercase"
+              className="flex-1 text-2xl md:text-3xl lg:text-4xl font-light text-black placeholder-gray-300 bg-transparent border-none outline-none uppercase"
               autoFocus={isSearchVisible}
             />
 
             <button
-              className="ml-4 text-black dark:text-white hover:text-gray-900 dark:hover:text-gray-300 transition-colors"
+              className="ml-4 text-black hover:text-gray-900 transition-colors"
               aria-label="Submit search"
               onClick={handleSearchSubmit}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256">
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256">
                 <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path>
               </svg>
             </button>
@@ -176,7 +271,7 @@ const Header = () => {
       </div>
 
       <div
-        className={`fixed top-[60px] lg:top-[70px] left-0 right-0 bottom-0 bg-white dark:bg-neutral-900 text-black dark:text-white z-40 transform transition-all duration-300 ease-in-out overflow-hidden ${
+        className={`fixed top-[60px] lg:top-[70px] left-0 right-0 bottom-0 bg-white text-black z-40 transform transition-all duration-300 ease-in-out overflow-hidden ${
           isNavVisible ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0 pointer-events-none"
         }`}
         ref={navRef}
@@ -186,35 +281,35 @@ const Header = () => {
           {/* Main Navigation Links */}
           <Link
             href="/"
-            className="flex items-center justify-center text-sans-35 font-600 border-b border-black dark:border-neutral-700 border-solid hover:italic py-5"
+            className="flex items-center justify-center text-sans-35 font-600 border-b border-black border-solid hover:italic py-5"
             onClick={handleLinkClick}
           >
             START
           </Link>
           <Link
             href="/om-oss"
-            className="flex items-center justify-center text-sans-35 font-600 border-b border-black dark:border-neutral-700 border-solid hover:italic py-5"
+            className="flex items-center justify-center text-sans-35 font-600 border-b border-black border-solid hover:italic py-5"
             onClick={handleLinkClick}
           >
             OM OSS
           </Link>
           <Link
             href="/artists"
-            className="flex items-center justify-center text-sans-35 font-600 border-b border-black dark:border-neutral-700 border-solid hover:italic py-5"
+            className="flex items-center justify-center text-sans-35 font-600 border-b border-black border-solid hover:italic py-5"
             onClick={handleLinkClick}
           >
             ARTISTER
           </Link>
           <Link
             href="/edits"
-            className="flex items-center justify-center text-sans-35 font-600 border-b border-black dark:border-neutral-700 border-solid hover:italic py-5"
+            className="flex items-center justify-center text-sans-35 font-600 border-b border-black border-solid hover:italic py-5"
             onClick={handleLinkClick}
           >
             EDITS
           </Link>
           <Link
             href="/event"
-            className="flex items-center justify-center text-sans-35 font-600 border-b border-black dark:border-neutral-700 border-solid hover:italic py-5"
+            className="flex items-center justify-center text-sans-35 font-600 border-b border-black border-solid hover:italic py-5"
             onClick={handleLinkClick}
           >
             EVENTS
@@ -270,18 +365,18 @@ const Header = () => {
         </div>
 
         {/* Desktop: Keep the original grid layout */}
-        <div className="hidden lg:grid grid-cols-2 h-[calc(100vh-70px)] border-b border-black dark:border-neutral-700 border-solid">
+        <div className="hidden lg:grid grid-cols-2 h-[calc(100vh-70px)] border-b border-black border-solid">
           {/* Top row */}
           <Link
             href="/"
-            className="flex items-center justify-center text-sans-60 font-600 border-r border-b border-black dark:border-neutral-700 border-solid hover:italic"
+            className="flex items-center justify-center text-sans-60 font-600 border-r border-b border-black border-solid hover:italic"
             onClick={handleLinkClick}
           >
             START
           </Link>
           <Link
             href="/om-oss"
-            className="flex items-center justify-center text-sans-60 font-600 border-b border-black dark:border-neutral-700 border-solid hover:italic"
+            className="flex items-center justify-center text-sans-60 font-600 border-b border-black border-solid hover:italic"
             onClick={handleLinkClick}
           >
             OM OSS
@@ -290,21 +385,21 @@ const Header = () => {
           {/* Middle row */}
           <Link
             href="/artists"
-            className="flex items-center justify-center text-sans-60 font-600 border-r border-b border-black dark:border-neutral-700 border-solid hover:italic"
+            className="flex items-center justify-center text-sans-60 font-600 border-r border-b border-black border-solid hover:italic"
             onClick={handleLinkClick}
           >
             ARTISTER
           </Link>
           <Link
             href="/edits"
-            className="flex items-center justify-center text-sans-60 font-600 border-b border-black dark:border-neutral-700 border-solid hover:italic"
+            className="flex items-center justify-center text-sans-60 font-600 border-b border-black border-solid hover:italic"
             onClick={handleLinkClick}
           >
             EDITS
           </Link>
 
           {/* Bottom row with lucky button */}
-          <div className="relative flex items-center justify-center text-sans-60 font-600 border-r border-black dark:border-neutral-700 border-solid hover:italic">
+          <div className="relative flex items-center justify-center text-sans-60 font-600 border-r border-black border-solid hover:italic">
             <Link href="/event" className="w-full h-full flex items-center justify-center" onClick={handleLinkClick}>
               EVENTS
             </Link>
